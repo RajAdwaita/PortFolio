02f474e507c7fd4654bd9fd207fcbf28b8865a8d
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-05-26 02:29:09 +0530

    Started mq in experience section

--- portfolio/src/Components/experience/Experience.jsx
+++ portfolio/src/Components/experience/Experience.jsx
@@ -9,71 +9,92 @@ const Experience = () => {
             <h5>My Skills</h5>
             <h2>Experience</h2>
 
-            <div className="container container_experience">
+            <div className="container experience_container">
                 <div className="experience_frontend">
                     <h3>FroneEnd Development</h3>
                     <div className="experience_content">
                         <article className='experience_details'>
-                            <FaReact />
-                            <h4>React</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <FaReact className='experience_details_icon' />
+                            <div>
+                                <h4>React</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
                         </article>
                         <article className='experience_details'>
-                            <FaReact />
-                            <h4>React</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <FaReact className='experience_details_icon' />
+                            <div>
+                                <h4>React</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
                         </article>
                         <article className='experience_details'>
-                            <FaReact />
-                            <h4>React</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <FaReact className='experience_details_icon' />
+                            <div>
+                                <h4>React</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
                         </article>
                         <article className='experience_details'>
-                            <FaReact />
-                            <h4>React</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <FaReact className='experience_details_icon' />
+                            <div>
+                                <h4>React</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
                         </article>
                         <article className='experience_details'>
-                            <FaReact />
-                            <h4>React</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <FaReact className='experience_details_icon' />
+                            <div>
+                                <h4>React</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
                         </article>
-
-                    </div>
-                    <div className="experience_backend">
-                        <h3>BackEnd Development</h3><div className="experience_content">
-                            <article className='experience_details'>
-                                <DiMongodb />
-                                <h4>MongoDb</h4>
-                                <small className='text-light'>Noob</small>
-                            </article>
-                            <article className='experience_details'>
-                                <DiMongodb />
-                                <h4>MongoDb</h4>
-                                <small className='text-light'>Noob</small>
-                            </article>
-                            <article className='experience_details'>
-                                <DiMongodb />
-                                <h4>MongoDb</h4>
-                                <small className='text-light'>Noob</small>
-                            </article>
-                            <article className='experience_details'>
-                                <DiMongodb />
-                                <h4>MongoDb</h4>
-                                <small className='text-light'>Noob</small>
-                            </article>
-                            <article className='experience_details'>
-                                <DiMongodb />
-                                <h4>MongoDb</h4>
-                                <small className='text-light'>Noob</small>
-                            </article>
-
-
-
-                        </div>
 
                     </div>
                 </div>
+                <div className="experience_backend">
+                    <h3>BackEnd Development</h3><div className="experience_content">
+                        <article className='experience_details'>
+                            <DiMongodb className='experience_details_icon' />
+                            <div>
+                                <h4>MongoDb</h4>
+                                <small className='text-light'>Noob</small>
+                            </div>
+                        </article>
+                        <article className='experience_details'>
+                            <DiMongodb className='experience_details_icon' />
+                            <div>
+                                <h4>MongoDb</h4>
+                                <small className='text-light'>Noob</small>
+                            </div>
+                        </article>
+                        <article className='experience_details'>
+                            <DiMongodb className='experience_details_icon' />
+                            <div>
+                                <h4>MongoDb</h4>
+                                <small className='text-light'>Noob</small>
+                            </div>
+                        </article>
+                        <article className='experience_details'>
+                            <DiMongodb className='experience_details_icon' />
+                            <div>
+                                <h4>MongoDb</h4>
+                                <small className='text-light'>Noob</small>
+                            </div>
+                        </article>
+                        <article className='experience_details'>
+                            <DiMongodb className='experience_details_icon' />
+                            <div>
+                                <h4>MongoDb</h4>
+                                <small className='text-light'>Noob</small>
+                            </div>
+                        </article>
+
+
+
+                    </div>
+
+                </div>
+
             </div>
         </section>
     )
