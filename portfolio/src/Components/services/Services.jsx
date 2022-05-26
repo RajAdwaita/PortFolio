193cbf5eb713951bea90f1be6710e7dd685ad6b3
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-05-26 20:45:27 +0530

    Services Section Done

--- portfolio/src/Components/services/Services.jsx
+++ portfolio/src/Components/services/Services.jsx
@@ -1,8 +1,108 @@
 import React from 'react'
 import "./services.css"
+import { AiOutlineCheck } from "react-icons/ai";
+
 const Services = () => {
     return (
-        <section id='services'>Services</section>
+        <section id='services'>
+            <h5>Adwaita</h5>
+            <h2>Modak</h2>
+            <div className="container services_container">
+                <article className="service">
+                    <div className="service_head">
+                        <h3>Web Developer</h3>
+                    </div>
+                    <ul className="service_list">
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className="service">
+                    <div className="service_head">
+                        <h3>Competitive Coding</h3>
+                    </div>
+                    <ul className="service_list">
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className="service">
+                    <div className="service_head">
+                        <h3>Football</h3>
+                    </div>
+                    <ul className="service_list">
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                        <li>
+                            <AiOutlineCheck className='service_list_icon' />
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, facere.</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
     )
 }
 
